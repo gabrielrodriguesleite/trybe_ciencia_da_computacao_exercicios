1d2b41ef205f60aba0487ac200352869e9406e34
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-02 17:34:43 -0300

    [feat] python set

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/games_report.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/games_report.py
@@ -13,4 +13,20 @@ except FileNotFoundError:
 finally:
     print("sempre executa finally")
 
-print(type(video_games))
+# print(type(video_games))
+# print(video_games[2]["Metadata"]["Genres"].split(','))
+
+# generos
+game_genres = set()
+for game in video_games:
+    for genre in game["Metadata"]["Genres"].split(','):
+        game_genres.add(genre)
+
+print(game_genres)
+
+# consoles
+consoles = set()
+for game in video_games:
+    consoles.add(game["Release"]["Console"])
+
+print(consoles)
