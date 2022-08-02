82b6c494b8e6387780c3d6a58ae5a98aa7386e2d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-02 16:58:08 -0300

    [feat] tratamento de erros

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/games_report.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-2-entrada-e-saida-de-dados/games_report.py
@@ -3,8 +3,14 @@ import json
 # json.load carrega do arquivo
 # json.loads carrega de variavel
 
-with open("data/video_games.json", "r") as file:
-    video_games = json.load(file)
-
+try:
+    with open("data/video_games.json", "r") as file:
+        video_games = json.load(file)
+except json.decoder.JSONDecodeError:
+    print("Arquivo inválido")
+except FileNotFoundError:
+    print("Arquivo inexistente")
+finally:
+    print("sempre executa finally")
 
 print(type(video_games))
