45f72c12ac9c79907bb8cb28e50399b09288930c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-03 23:33:08 -0300

    [feat] exercicio2 ok

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-3-testes/exercicio-02.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-3-testes/exercicio-02.py
@@ -1,6 +1,28 @@
 def decodeNumber(frase):
+    code = ""
+    for letter in frase:
+        if letter in "-":
+            code += "-"
+        if letter in "1":
+            code += "1"
+        if letter in "ABC":
+            code += "2"
+        if letter in "DEF":
+            code += "3"
+        if letter in "GHI":
+            code += "4"
+        if letter in "JKL":
+            code += "5"
+        if letter in "MNO":
+            code += "6"
+        if letter in "PQRS":
+            code += "7"
+        if letter in "TUV":
+            code += "8"
+        if letter in "WXYZ":
+            code += "9"
 
-    return "1-4663-79338-4663"
+    return code
 
 
 def test_decodeNumber_1():
