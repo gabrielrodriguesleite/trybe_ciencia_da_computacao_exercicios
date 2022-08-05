564dc968b8d996cdae8961be887f692c50d7d807
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-04 23:45:27 -0300

    [feat] bonus01 testes

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/bonus01.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/bonus01.py
@@ -11,4 +11,12 @@ def minimum(list):
 
 
 def test_minimum():
-    assert minimum( [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]) == 2
+    assert minimum([5, 9, 3, 19, 70, 8, 100, 2, 35, 27]) == 2
+
+
+def test_minimum2():
+    assert minimum([5, 9, 3, 19, 70, 8, 100, 35, 27]) == 3
+
+
+def test_minimum3():
+    assert minimum([5, 9, 19, 70, 8, 100, 35, 27]) == 5
