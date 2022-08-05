9670ce4c18e8176d0bf7c611ad71526987f3d0af
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-04 23:30:10 -0300

    [feat] exercicio 6 isóceles

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercicio06.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-1-aprendendo-python/exercicio06.py
@@ -1,5 +1,7 @@
 def trigangulate(a, b, c):
     if a == b and b == c and c == a:
-        return "Isóceles"
+        return "Equilátero"
     if a != b and b != c and c != a:
         return "Escaleno"
+    if a + b == c or b + c == a or a + c == b:
+        return "Isóceles"
