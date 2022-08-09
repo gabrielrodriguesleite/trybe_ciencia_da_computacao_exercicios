35d81df3f31c866e85d5091312c309edd4a4008d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-08 22:57:24 -0300

    [feat] exercicio 1.2

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio01.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio01.py
@@ -7,3 +7,6 @@ class television:
 
   def aumentar_volume(self):
     self.volume = 99 if self.volume > 100 else self.volume + 1
+
+  def diminuir_volume(self):
+    self.volume = 0 if self.volume < 0 else self.volume - 1
