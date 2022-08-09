336814f363ad9af4ded2da3d301377cfdcc69e48
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-08 23:02:05 -0300

    [fix] identação corrigida

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio01.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio01.py
@@ -1,20 +1,20 @@
 class television:
-  def __init__(self, tamanho):
-    self.volume = 50
-    self.canal = 1
-    self.tamanho = tamanho
-    self.ligada = False
+    def __init__(self, tamanho):
+        self.volume = 50
+        self.canal = 1
+        self.tamanho = tamanho
+        self.ligada = False
 
-  def aumentar_volume(self):
-    self.volume = 99 if self.volume > 100 else self.volume + 1
+    def aumentar_volume(self):
+        self.volume = 99 if self.volume > 100 else self.volume + 1
 
-  def diminuir_volume(self):
-    self.volume = 0 if self.volume < 0 else self.volume - 1
+    def diminuir_volume(self):
+        self.volume = 0 if self.volume < 0 else self.volume - 1
 
-  def modificar_canal(self, canal):
-    if 1 > canal > 99:
-      raise ValueError
-    self.canal = canal
+    def modificar_canal(self, canal):
+        if 1 > canal > 99:
+            raise ValueError
+        self.canal = canal
 
-  def ligar_desligar(self):
-    self.ligada = not self.ligada
+    def ligar_desligar(self):
+        self.ligada = not self.ligada
