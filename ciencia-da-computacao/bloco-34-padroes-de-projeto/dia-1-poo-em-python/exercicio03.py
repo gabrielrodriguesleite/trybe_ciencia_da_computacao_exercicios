371ce27ec40994f1df0f4332575976e7e1574184
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-15 22:58:23 -0300

    [feat] exercicio 3 classes geometricas | quadrado perimetro | testes

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio03.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio03.py
@@ -3,9 +3,11 @@ from abc import ABC
 
 
 class geometryAbs(ABC):
+    @classmethod
     def area(self):
         pass
 
+    @classmethod
     def perimetro(self):
         pass
 
@@ -17,11 +19,18 @@ class quadrado(geometryAbs):
 
     def area(self):
         return self.lado * self.lado
+    
+    def perimetro(self):
+        return self.lado * 4
 
 
 def test_quadrado_area():
     assert quadrado(4).area() == 16
     assert quadrado(11).area() == 121
+    
+def test_quadrado_perimetro():
+    assert quadrado(5).perimetro() == 20
+    assert quadrado(11).perimetro() == 44
 
 
 if __name__ == '__main__':
