5b5b20ba964f6f3b4c18e011b58e0e0ca7459b80
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-15 23:30:01 -0300

    [feat] exercicio 4 classe log em arquivo

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
@@ -5,4 +5,10 @@ class ManipuladorDeLog(ABC):
     @abstractmethod
     def log(self):
         pass
-      
+
+
+class LogEmArquivo(ManipuladorDeLog):
+    def log(self, message):
+        print("print em arquivo")
+        
+        
