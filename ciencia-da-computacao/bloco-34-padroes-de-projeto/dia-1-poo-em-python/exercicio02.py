11ef7faea9d353a5b4f6b584575be64a5f5ee220
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 07:31:16 -0300

    [feat] exercicios 34.2 lint

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio02.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio02.py
@@ -1,5 +1,3 @@
-import pytest
-
 class Estatistica:
     def media(self, lista):
         """Calculo de média aritimética dos números de uma lista
@@ -22,7 +20,8 @@ class Estatistica:
             lista (array de números): um array de números
 
         Returns:
-            número: O valor central de um conjunto ordenado ou a média dos 2 valores centrais
+            número: O valor central de um conjunto ordenado ou a média dos \
+                2 valores centrais
         """
         lista.sort()
         if len(lista) % 2 == 0:
@@ -35,9 +34,10 @@ class Estatistica:
 
         Args:
             lista (array de números): um array de números
-            
+
         Returns:
-            número: O número que aparece com mais frequência em um conjunto de dados
+            número: O número que aparece com mais frequência em um conjunto \
+                de dados
         """
         lista.sort()
         m = {}
@@ -52,21 +52,24 @@ class Estatistica:
                 o = m[j]
         return [f for f in m if m[f] == o][0]
 
+
 def test_estatistica_media():
-    assert Estatistica().media([1,2,3]) == 2.0
-    assert Estatistica().media([5,4,9]) == 6.0
-    
+    assert Estatistica().media([1, 2, 3]) == 2.0
+    assert Estatistica().media([5, 4, 9]) == 6.0
+
+
 def test_estatistica_mediana():
-    assert Estatistica().mediana([1,2,3]) == 2
-    assert Estatistica().mediana([31,6,4,9]) == 20.0
-    
+    assert Estatistica().mediana([1, 2, 3]) == 2
+    assert Estatistica().mediana([31, 6, 4, 9]) == 20.0
+
+
 def test_estatistica_moda():
-    assert Estatistica().moda([1,1,2,3,3,3,3,4,4,5,5,5]) == 3
-    assert Estatistica().moda([155,13,22,33,35,35,34,4,446,56,56,35]) == 35
-    
+    assert Estatistica().moda([1, 1, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5]) == 3
+    assert Estatistica().moda(
+        [155, 13, 22, 33, 35, 35, 34, 4, 446, 56, 56, 35]) == 35
+
 
 if __name__ == "__main__":
     # print(Estatistica().mediana([31,6,4,9]))
     # print(Estatistica().mediana([1,2,3]))
-    print(Estatistica().moda([1,1,2,3,3,3,3,4,4,5,5,5]))
-    
+    print(Estatistica().moda([1, 1, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5]))
