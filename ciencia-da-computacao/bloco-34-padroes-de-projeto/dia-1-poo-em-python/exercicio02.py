07186cd9f354912518533fed85cbc75564158f49
gabrielrodriguesleite <k.cogabriel@gmail.com>
2022-08-15 21:49:04 -0300

    [feat] media

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio02.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio02.py
@@ -1,6 +1,12 @@
+from re import I
+
+
 class Estatistica:
-    def media(self):
-        pass
+    def media(self, lista):
+        sum = 0
+        for i in lista:
+            sum += i
+        return sum
 
     def mediana(self):
         pass
