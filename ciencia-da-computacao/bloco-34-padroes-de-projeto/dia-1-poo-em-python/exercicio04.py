0b45077975a0d0ae57b777a0347b2a49bacc086f
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 07:33:01 -0300

    [feat] exercicio 4 raise error for abstract method

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 class ManipuladorDeLog(ABC):
     @abstractmethod
     def log(self):
-        pass
+        raise NotImplementedError
 
 
 class LogEmArquivo(ManipuladorDeLog):
