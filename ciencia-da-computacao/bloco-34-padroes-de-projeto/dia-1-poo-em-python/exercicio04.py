5b7c400491cf8e15a3885194e0b07e3682476d9b
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-16 07:46:42 -0300

    [feat] exercicio 4 | classe com metodo abstrato implementada

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/exercicio04.py
@@ -11,9 +11,34 @@ class ManipuladorDeLog(ABC):
 
 class LogEmArquivo(ManipuladorDeLog):
     def log(self, message):
-        print("log em arquivo")
+        print("log em arquivo: ", message)
 
 
 class LogEmTela(ManipuladorDeLog):
     def log(self, message):
-        print("log em tela")
+        print("log em tela: ", message)
+
+
+class Log():
+    def __init__(self) -> None:
+        self.manipuladores = []
+
+    def adicionar_manipulador(self, manipulador):
+        if manipulador not in self.manipuladores:
+            self.manipuladorer.push(manipulador)
+
+    def info(self, message):
+        for m in self.manipuladores:
+            m.log(message)
+
+    def alerta(self, message):
+        for m in self.manipuladores:
+            m.log(message)
+
+    def erro(self, message):
+        for m in self.manipuladores:
+            m.log(message)
+
+    def debug(self, message):
+        for m in self.manipuladores:
+            m.log(message)
