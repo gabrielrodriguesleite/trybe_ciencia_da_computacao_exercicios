90f23ca84c1fed7873ae0079a39bc7d9a078730c
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-17 22:49:12 -0300

    [feat] servidor tcp simples funcional | testado no navegador

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-1-arquitetura-de-redes-ferramentas-e-seguranca/simple_servidor_tcp.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-1-arquitetura-de-redes-ferramentas-e-seguranca/simple_servidor_tcp.py
@@ -4,9 +4,16 @@ from socketserver import StreamRequestHandler, TCPServer
 class TCPHandler(StreamRequestHandler):
     def handle(self):  # Método abstrato de StreamRequestHandler
         self.wfile.write(b'Ola, Cliente!\n')
+        while True:
+            data = self.rfile.readline().strip().decode('UTF-8')
+            if not data:
+                self.wfile.write(b'Cliente desconectado')
+                print("Cliente foi desconectado")
+                break
+            print(data)
 
 
 if __name__ == "__main__":
-    server_address = ("localhost", "8080")
+    server_address = ("localhost", 8080)
     with TCPServer(server_address, TCPHandler) as server:
         server.serve_forever()
