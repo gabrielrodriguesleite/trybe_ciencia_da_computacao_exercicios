53b342eb6b1408d22abe8af4c54223b2920eec2e
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-17 23:54:18 -0300

    [feat] exercicio06 ICMS ok

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
@@ -41,6 +41,6 @@ class Orcamento:
 
 orcamento = Orcamento(1000)
 print(f"ISS: {orcamento.calcular_imposto(ISS)}")
-# print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+print(f"ICMS: {orcamento.calcular_imposto(ICMS)}")
 # print(f"PIS: {orcamento.calcular_imposto('PIS')}")
 # print(f"COFINS: {orcamento.calcular_imposto('COFINS')}")
