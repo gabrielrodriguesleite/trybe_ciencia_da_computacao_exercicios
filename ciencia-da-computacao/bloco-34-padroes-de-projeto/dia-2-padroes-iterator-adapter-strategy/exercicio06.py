86147028db5ebd9b87684a27682ed5dac18bb1fe
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-17 23:53:34 -0300

    [feat] exercicio06 class Orcamento modificada para funcionar com classmethod

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
@@ -36,30 +36,11 @@ class Orcamento:
         self.valor = valor
 
     def calcular_imposto(self, imposto):
-        if imposto == 'ISS':
-            return self.__calcular_iss()
-        elif imposto == 'ICMS':
-            return self.__calcular_icms()
-        elif imposto == 'PIS':
-            return self.__calcular_pis()
-        elif imposto == 'COFINS':
-            return self.__calcular_cofins()
-
-    def __calcular_iss(self):
-        return self.valor * 0.1
-
-    def __calcular_icms(self):
-        return self.valor * 0.06
-
-    def __calcular_pis(self):
-        return self.valor * 0.0065
-
-    def __calcular_cofins(self):
-        return self.valor * 0.03
+        return imposto.calcular(self.valor)
 
 
 orcamento = Orcamento(1000)
-print(f"ISS: {orcamento.calcular_imposto('ISS')}")
-print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
-print(f"PIS: {orcamento.calcular_imposto('PIS')}")
-print(f"COFINS: {orcamento.calcular_imposto('COFINS')}")
+print(f"ISS: {orcamento.calcular_imposto(ISS)}")
+# print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+# print(f"PIS: {orcamento.calcular_imposto('PIS')}")
+# print(f"COFINS: {orcamento.calcular_imposto('COFINS')}")
