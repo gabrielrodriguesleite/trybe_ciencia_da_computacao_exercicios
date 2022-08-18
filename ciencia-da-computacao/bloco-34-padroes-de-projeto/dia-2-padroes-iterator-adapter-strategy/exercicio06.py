0e35ccf6300e6512c078cd474f69c2eddd94f146
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-17 23:45:25 -0300

    [feat] exercicio06 cofins added

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercicio06.py
@@ -9,6 +9,8 @@ class Orcamento:
             return self.__calcular_icms()
         elif imposto == 'PIS':
             return self.__calcular_pis()
+        elif imposto == 'COFINS':
+            return self.__calcular_cofins()
 
     def __calcular_iss(self):
         return self.valor * 0.1
@@ -19,8 +21,12 @@ class Orcamento:
     def __calcular_pis(self):
         return self.valor * 0.0065
 
+    def __calcular_cofins(self):
+        return self.valor * 0.03
+
 
 orcamento = Orcamento(1000)
 print(f"ISS: {orcamento.calcular_imposto('ISS')}")
 print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
 print(f"PIS: {orcamento.calcular_imposto('PIS')}")
+print(f"COFINS: {orcamento.calcular_imposto('COFINS')}")
