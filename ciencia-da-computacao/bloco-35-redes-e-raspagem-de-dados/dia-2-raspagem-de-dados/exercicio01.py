86abadd27a3009d9cd5ab2b91dc5a5bc097b9e80
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 22:43:30 -0300

    [feat] black exercicio 01

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio01.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio01.py
@@ -2,9 +2,9 @@ import requests
 
 
 def main():
-    data = requests.get('https://httpbin.org/encoding/utf8')
+    data = requests.get("https://httpbin.org/encoding/utf8")
     print(data.text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
