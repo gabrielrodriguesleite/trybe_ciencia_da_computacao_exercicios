b717c628285442eeb3568bf7bf185f415dfd3097
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:48:51 -0300

    [feat] exercicio 06 sem mongo ok

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio06-semMongo.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio06-semMongo.py
@@ -5,4 +5,10 @@ FILE = (
     "ciencia-da-computacao/bloco-35-redes-e-"
     "raspagem-de-dados/dia-2-raspagem-de-dados/books.json")
 with open(FILE) as file:
-    print(json.load(file))
+    books = json.load(file)
+
+category = "Java"
+
+for book in books:
+    if category in book["categories"]:
+        print(book["title"])
