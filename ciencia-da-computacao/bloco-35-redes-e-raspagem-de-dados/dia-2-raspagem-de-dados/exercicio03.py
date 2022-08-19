c7ce87ec87ee8439a3a911a3e0ab19fdddd0f24e
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 22:53:42 -0300

    [feat] exercicio 03 com pytest

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio03.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio03.py
@@ -1,8 +1,11 @@
 import requests
 
-resp = requests.get(
-    "https://scrapethissite.com/pages/advanced/?gotcha=headers",
-    headers={"User-agent": "Mozilla", "Accept": "text/html"},
+
+def test_bot_detected():
+    response = requests.get(
+        "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+        headers={"User-agent": "Mozilla", "Accept": "text/html"},
     )
-print(resp.text)
-assert "bot detected" not in resp.text
+    assert "bot detected" not in response.text
+
+# run with pytest
