814a35c2aae12f62fc2d927d5c6fbe05210830a4
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-19 10:30:35 -0300

    [feat] exercicio bonus 08 download images

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio08bonus.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio08bonus.py
@@ -1,7 +1,37 @@
+import requests
+from parsel import Selector
+
+
+def crawl(url) -> requests:
+    return requests.get(
+        url,
+        headers={"User-agent": "Mozilla", "Accept": "text/html"})
+
+
+def get_flags_urls() -> list:
+    URL = "https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags"
+    galeryResponse = crawl(URL)
+    if galeryResponse.status_code == 200:
+        sel = Selector(galeryResponse.text)
+        flagssrc = sel.css("a.image > img::attr(src)").getall()
+        flags = [f"https:{f}" for f in flagssrc]
+        # print(flags[0])
+        return flags
+
+
+def save_image_from_url(url):
+    # https://stackoverflow.com/questions/13137817/how-to-download-image-using-requests
+    r = requests.get(url, stream=True)
+    with open(url.split('/')[-1], mode="wb") as file:
+        for chunk in r:
+            file.write(chunk)
+
 
 def main():
-
-    return
+    # print(crawl(get_flags_urls()[0]).)
+    # save_image_from_url(get_flags_urls()[0])
+    for i in get_flags_urls():
+        save_image_from_url(i)
 
 
 if __name__ == "__main__":
