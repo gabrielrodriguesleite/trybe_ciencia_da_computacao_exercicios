0083c7f3117e00347754cba6e49899414f8e1cde
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:59:36 -0300

    [feat] exercicio 07 rank categories sem sort

---
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio07.py
@@ -0,0 +1,25 @@
+import json
+
+
+def rank_published_books():
+
+    FILE = (
+        "ciencia-da-computacao/bloco-35-redes-e-"
+        "raspagem-de-dados/dia-2-raspagem-de-dados/books.json")
+    with open(FILE) as file:
+        books = json.load(file)
+
+    rank = {}
+    for book in books:
+        if book["status"] == "PUBLISH":
+            for cat in book["categories"]:
+                if cat not in rank:
+                    rank[cat] = 1
+                else:
+                    rank[cat] += 1
+
+    print(rank)
+
+
+if __name__ == "__main__":
+    rank_published_books()
