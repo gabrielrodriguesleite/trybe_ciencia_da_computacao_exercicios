c9ec4cec98cf8c0ff3982d506f308e9d8b4d4589
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:20:13 -0300

    [feat] exercicio 04 description

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     sel = Selector(data)
     title = sel.css("div.col-sm-6 > h1::text").get()
     price = sel.css("p.price_color::text").re_first(r"\d+\.\d{2}")
-    description = ""
+    description = sel.css("article > p::text").get()
+    description = description[:-len(" ...more")]
     image_url = ""
     print(title, price, description, image_url, sep=",")
