e966ce9b9adf0cfe4e224bba321bae11f1d5656d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:51:26 -0300

    [feat] exercicio 06 sem mongo aceita input

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio06-semMongo.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio06-semMongo.py
@@ -1,14 +1,21 @@
 import json
 
 
-FILE = (
-    "ciencia-da-computacao/bloco-35-redes-e-"
-    "raspagem-de-dados/dia-2-raspagem-de-dados/books.json")
-with open(FILE) as file:
-    books = json.load(file)
+def list_book_category(category):
 
-category = "Java"
+    FILE = (
+        "ciencia-da-computacao/bloco-35-redes-e-"
+        "raspagem-de-dados/dia-2-raspagem-de-dados/books.json")
+    with open(FILE) as file:
+        books = json.load(file)
 
-for book in books:
-    if category in book["categories"]:
-        print(book["title"])
+    # category = "Java"
+
+    for book in books:
+        if category in book["categories"]:
+            print(book["title"])
+
+
+if __name__ == "__main__":
+    print("Digite uma categoria:")
+    list_book_category(input())
