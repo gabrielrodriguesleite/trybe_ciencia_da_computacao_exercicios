3055f48e80633e36ea018f43a16f55242dd6bdd8
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 22:50:24 -0300

    [feat] exercicio 03 "bot detected" corrected

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio03.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio03.py
@@ -1,6 +1,8 @@
 import requests
 
 resp = requests.get(
-    "https://scrapethissite.com/pages/advanced/?gotcha=headers")
+    "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+    headers={"User-agent": "Mozilla", "Accept": "text/html"},
+    )
 print(resp.text)
 assert "bot detected" not in resp.text
