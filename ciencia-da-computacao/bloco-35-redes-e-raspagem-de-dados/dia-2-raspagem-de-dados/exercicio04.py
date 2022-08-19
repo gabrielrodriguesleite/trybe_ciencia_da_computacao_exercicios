bc2a6648f50734265edbd9299795d8d915eaf061
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:06:55 -0300

    [feat] exercicio 04 title

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
@@ -1,7 +1,21 @@
 import requests
+from parsel import Selector
 
 
 def crawl(url):
     return requests.get(
         url,
         headers={"User-agent": "Mozilla", "Accept": "text/html"})
+
+
+if __name__ == "__main__":
+    data = crawl(
+        "http://books.toscrape.com/catalogue/"
+        "the-grand-design_405/index.html"
+    ).text
+    sel = Selector(data)
+    title = sel.css("div.col-sm-6 > h1::text").get()
+    price = ""
+    description = ""
+    image_url = ""
+    print(title, price, description, image_url)
