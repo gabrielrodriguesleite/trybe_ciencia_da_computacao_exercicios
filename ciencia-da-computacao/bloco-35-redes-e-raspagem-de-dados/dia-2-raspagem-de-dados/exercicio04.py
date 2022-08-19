90fb5d57d04daffdcd7f6bf8a3016b1fb2a0025b
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 23:29:08 -0300

    [feat] exercicio 04 thumbnail

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio04.py
@@ -9,14 +9,15 @@ def crawl(url):
 
 
 if __name__ == "__main__":
-    data = crawl(
+    BASE_URL = (
         "http://books.toscrape.com/catalogue/"
         "the-grand-design_405/index.html"
-    ).text
+        )
+    data = crawl(BASE_URL).text
     sel = Selector(data)
     title = sel.css("div.col-sm-6 > h1::text").get()
     price = sel.css("p.price_color::text").re_first(r"\d+\.\d{2}")
     description = sel.css("article > p::text").get()
     description = description[:-len(" ...more")]
-    image_url = ""
+    image_url = BASE_URL + sel.css("div.thumbnail img::attr(src)").get()
     print(title, price, description, image_url, sep=",")
