6fc209435022e2a4492068851fdebd54d033931d
Gabriel Rodrigues Leite <k.cogabriel@gmail.com>
2022-08-18 22:41:39 -0300

    [feat] exercicio 02 ok

---
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/exercicio02.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def main():
+    data = requests.get('https://api.github.com/users')
+    if data.status_code == 200:
+        items = data.json()
+        for u in items:
+            print(f"{u['login']} {u['url']}")
+
+
+if __name__ == '__main__':
+    main()
